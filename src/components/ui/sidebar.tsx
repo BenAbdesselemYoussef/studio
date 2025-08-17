281c99859eb5a5e4b9a89ab76bd92c2da3758a89
Youssef Ben Abdesselem <youssefbenabdesselem@gmail.com>
2025-08-17 21:27:04 +0000

    can you move the dark mode/light mode switch to the right using the item

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -333,7 +333,7 @@ const SidebarFooter = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="footer"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col items-end gap-2 p-2", className)}
       {...props}
     />
   )
